758dcdb83af31f749b26b15ed746192808ddae01
Javier Prado <pradojavier930@gnail.com>
2024-09-19 22:48:43 -0500

    feature_resta terminado

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,25 @@
-numero1= float(input("ingrese el primer numero: "))
-numero2= float(input("ingrese el segundo numero: "))
 
 def suma(numero1, numero2):
     return numero1+numero2
 
-resultado= suma(numero1, numero2)
+def resta(num1,num2):
+    return num1-num2
+
+def calculadora():
+    print("Operacion a realizar:")
+    print("1. Sumar")
+    print("2. Restar")
+
+    opcion=int(input("Seleccione una operacion(1/2):"))
+    print("_________________________________")
+    numero1= float(input("ingrese el primer numero: "))
+    numero2= float(input("ingrese el segundo numero: "))
+
+    if(opcion==1):
+        return suma(numero1,numero2)
+    else:
+        return resta( numero1,numero2)
+
+    
+resultado= calculadora()
 print("valor optenido:",resultado)
