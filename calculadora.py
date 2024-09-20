b95dd0fe067d770c6d7843c3ab5ee843fac13254
MoisesTalavera <talaveraurriburumoises@gmail.com>
2024-09-19 22:29:46 -0500

    feature_suma terminado

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+numero1= float(input("ingrese el primer numero: "))
+numero2= float(input("ingrese el segundo numero: "))
+
+def suma(numero1, numero2):
+    return numero1+numero2
+
+resultado= suma(numero1, numero2)
+print("valor optenido:",resultado)
